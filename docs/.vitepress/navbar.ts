18e24c39f025c258fab6aa7881e0979f681a2729
Claroja <63183535@qq.com>
2025-10-07 17:40:58 +0800

    update

--- docs/.vitepress/navbar.ts
+++ docs/.vitepress/navbar.ts
@@ -1,4 +1,4 @@
 let navbar: any
 export default navbar =
-[{'text': '机器学习', 'link': '/index/1机器学习.md'}, {'text': '数据挖掘', 'link': '/index/2数据挖掘.md'}, {'text': '编程基础', 'link': '/index/3编程基础.md'}, {'text': '编程语言', 'link': '/index/4编程语言.md'}, {'text': '网络爬虫', 'link': '/index/5网络爬虫.md'}, {'text': '前端开发', 'link': '/index/6前端开发.md'}, {'text': '数据工程', 'link': '/index/8数据工程.md'}, {'text': '图像处理', 'link': '/index/9图像处理.md'}, {'text': '法学', 'link': '/index/10法学.md'}, {'text': '学习', 'link': '/index/11学习.md'}, {'text': '方法论', 'link': '/index/13方法论.md'}]
+[{'text': '机器学习', 'link': '/index/1机器学习.md'}, {'text': '数据挖掘', 'link': '/index/2数据挖掘.md'}, {'text': '编程基础', 'link': '/index/3编程基础.md'}, {'text': '编程语言', 'link': '/index/4编程语言.md'}, {'text': '网络爬虫', 'link': '/index/5网络爬虫.md'}, {'text': '前端开发', 'link': '/index/6前端开发.md'}, {'text': '后端开发', 'link': '/index/7后端开发.md'}, {'text': '数据工程', 'link': '/index/8数据工程.md'}, {'text': '图像处理', 'link': '/index/9图像处理.md'}, {'text': '法学', 'link': '/index/10法学.md'}, {'text': '学习', 'link': '/index/11学习.md'}, {'text': '方法论', 'link': '/index/13方法论.md'}]
 ;
